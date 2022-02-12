3ede2e91a255c2aa1e15cb52c04b27c3e65fec8b
weebler76 <99522012+weebler76@users.noreply.github.com>
2022-02-12 04:51:55 +0000

    printed json

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import json
 #get city or zip from user 
 location = input("Please enter city or zipcode. ")
 
-response= requests.get("http://api.openweathermap.org/data/2.5/find?q=London&appid=b7102f66d17a9d1548feb5470a21746e")
+response= requests.get("http://api.openweathermap.org/data/2.5/find?q=30721&appid=b7102f66d17a9d1548feb5470a21746e")
 
 #tell user if connected
 if response.status_code == 200:
@@ -14,3 +14,5 @@ if response.status_code == 200:
 
 else:
     print("You failed.")
+
+print(response.json())
