2fae1772c7a07904f9f2ebb759fc2fb45835b401
weebler76 <99522012+weebler76@users.noreply.github.com>
2022-02-12 05:17:50 +0000

    Put location variable in call

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ import json
 #get city or zip from user 
 location = input("Please enter city or zipcode. ")
 
-response= requests.get("http://api.openweathermap.org/data/2.5/find?q=30721&appid=b7102f66d17a9d1548feb5470a21746e")
+
+response= requests.get(f"http://api.openweathermap.org/data/2.5/find?q={location}&APPID=b7102f66d17a9d1548feb5470a21746e")
 
 #tell user if connected
 if response.status_code == 200:
