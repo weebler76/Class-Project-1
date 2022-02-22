72c26a8e156aa9fa5af011da2f98ee8f92c86026
weebler76 <99522012+weebler76@users.noreply.github.com>
2022-02-22 22:22:57 +0000

    API call and connection status

--- main.py
+++ main.py
@@ -8,12 +8,59 @@ def get_location():
         location = input("Please enter a city name or 5 digit zipcode. ")
         if (location.isnumeric() and len(location) == 5):
             inputIsGood = True
+            
         elif (location.isalpha()):
             inputIsGood = True
+
+        
         else:
             print ("\nIncorrect city or zip entered.")
             continue
+    
         return location
+        
+def main():
+    location = get_location()
+    try:
+        response = requests.get(f"htttp://api.openweathermap.org/data/2.5/find?q={location},US&APPID=b7102f66d17a9d1548feb5470a21746e")
+        print("Successfully connected to openweathmap.org.")
+    except:
+        print ("Failed to connect.")
+        restart = input("(R)estart or (Q)?")
+        case = restart.lower()
+        if case == "q":
+            print (case)
+        elif case == "r":
+            print(case)
+        else: 
+            print ("Gotta figure this one out.")
+            
+        
+        
+
+
+    
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+    
+
+
+
+
+
+
+
+
+
+
 
 
 
