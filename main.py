79c57d05acd830fec1bf9e33c31c31dcc7e7c3b3
weebler76 <99522012+weebler76@users.noreply.github.com>
2022-02-24 03:39:43 +0000

    Done

--- main.py
+++ main.py
@@ -3,28 +3,31 @@ import json
 
 
 def get_location():
-    '''Get and validate user info'''
-    inputIsGood = False
-    while inputIsGood == False:
-        location = input("Please enter a city name or 5 digit zipcode. ")
-        if (location.isnumeric() and len(location) == 5):
-            inputIsGood = True
-            
-        elif (location.isalpha()):
-            inputIsGood = True
-
-        
-        else:
-            print ("\nIncorrect city or zip entered.")
-            continue
+    '''Get city or zip from user'''
     
-        return location
-        
+    location = input("Please enter a city name or 5 digit zipcode. ")
+    return location
+
+
+def run_again():
+    '''Ask if user if they want to run again or exit'''
+    again = input("\nWould you like to see weather for another location? y or n ")
+    print('')
+    if again == "y":
+        main()
+    elif again =="n":
+        print('Goodbye.')
+        exit()
+    else:
+        print("Please enter y or n. ")
+        run_again()
+    
 def main():
+    #Connect to API and print connection status
     location = get_location()
     try:
         r = requests.get(f"http://api.openweathermap.org/data/2.5/find?q={location},US&APPID=b7102f66d17a9d1548feb5470a21746e")
-        print("\nSuccessfully connected to openweathmap.org.")
+        print("\nSuccessfully connected to openweathmap.org.\n")
     except:
         print ("Failed to connect.")
         restart = input("(R)estart or (Q)?")
@@ -33,33 +36,33 @@ def main():
             exit("Goodbye.")
         else:
             main()
-    
 
+#Print weather info
+#Try except block to validate user input from get_location function
     data = r.json()
-    print (f"\nCurrent weather for {location}" )
-
-    
-    temp = ((data['list'][0]['main']['temp']) -273.15) * 9/5 + 32
-    temp1 = int(temp)
-    print (f"Current temperature: {temp1}.")
-    humid = (data['list'][0]['main']['humidity'])
-    print (f"Humidity: {humid}%." )
-    w = (data['list'][0]['weather'][0]['description'])
-    print (f"Clouds: {w}")
-
-    again = input("\nWould you like to see weather for another location? y or n ")
-    if again == "y":
+    try:
+        temp = ((data['list'][0]['main']['temp']) -273.15) * 9/5 + 32
+        temp1 = int(temp)
+        print (f"Current temperature: {temp1}.")
+        humid = (data['list'][0]['main']['humidity'])
+        print (f"Humidity: {humid}%." )
+        w = (data['list'][0]['weather'][0]['description'])
+        print (f"Clouds: {w}")
+    except:
+        print(f"{location} is not a valid city or zip code.\n")
         main()
-    else:
-        exit()
+
     
     
+    run_again()    
 
 
    
     
 if __name__ == "__main__":
     main()
+
+
 
 
 
